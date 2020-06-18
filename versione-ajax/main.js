e220a9b38d1066874861c62f981c678ea4fe0e2b
nicola <nico.mamo88@gmail.com>
2020-06-18 18:10:13 +0200

    sistemato database, gitignore

--- versione-ajax/main.js
+++ versione-ajax/main.js
@@ -1,40 +1,40 @@
 $(document).ready(function(){
     var source = $("#hb-template").html();
-      var template = Handlebars.compile(source);
+    var template = Handlebars.compile(source);
 
 
 // chiamata ajax da database
-  $.ajax({
-    url: "database/dischi.php",
-    method: "GET",
-    success: function(dischi) {
-        //console.log(dischi)
-        for (var i = 0; i < dischi.length; i++) {
-            var oggetto = dischi[i];
-            var titolo = oggetto.title;
-            var autore = oggetto.author;
-            var anno   = oggetto.year;
-            var immagine = oggetto.poster;
-// preparo contesto per handlebar
-            var context = {
-        'poster' : immagine,
-        'title '    :titolo,
-        'name'     : autore,
-        'anno' : anno,
-    };
+      $.ajax({
+        url: "database/dischi.php",
+        method: "GET",
+        success: function(dischi) {
+            console.log(dischi)
+            for (var i = 0; i < dischi.length; i++) {
+                var oggetto = dischi[i];
+                var titolo = oggetto.title;
+                var autore = oggetto.author;
+                var anno   = oggetto.year;
+                var immagine = oggetto.poster;
+    // preparo contesto per handlebar
+                var context = {
+                    'poster' : immagine,
+                    'title '    :titolo,
+                    'name'     : autore,
+                    'anno' : anno,
+                };
 
 
-          var html = template(context);
-          $(".cds-container").append(html)
+                var html = template(context);
+                $(".cds-container").append(html)
 
-    }//fine ciclo-for
+        }//fine ciclo-for
 
 
-    },
-    error: function() {
-        alert('errore')
-}
-});//fine ajax
+        },
+        error: function() {
+            alert('errore')
+        }
+    });//fine ajax
 
 
 });//fine document ready
