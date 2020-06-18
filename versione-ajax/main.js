02b484cdfddab9b1711bd6e5b4933cd0b4e4bf70
nicola <nico.mamo88@gmail.com>
2020-06-18 15:38:20 +0200

    problema array

--- versione-ajax/main.js
+++ versione-ajax/main.js
@@ -7,15 +7,36 @@ $(document).ready(function(){
     url: "database/dischi.php",
     method: "GET",
     success: function(dischi) {
-        var dischi = dischi.response
-        console.log(dischi)
+        //console.log(dischi)
+        var dischi = dischi.respsponse;
+        for (var i = 0; i < dischi.length; i++) {
+            var oggetto = dischi[i];
+            var titolo = dischi.title;
+            var autore = dischi.author;
+            var anno   = dischi.year;
+            var immagine = dischi.poster;
+// preparo contesto per handlebar
+            var context = {
+        'poster' : immagine,
+        'title '    :titolo,
+        'name'     : autore,
+        'anno' : anno,
+    };
+        var source = $("#hb-template").html();
+          var template = Handlebars.compile(source);
 
+          var html = template(context);
+          $(".cds-container").append(html)
+
+    }//fine ciclo-for
 
 
     },
     error: function() {
         alert('errore')
-    }
-  });
-});
+}
+});//fine ajax
+
+
+});//fine document ready
 // recupero dischi restituiti dall api (array)
